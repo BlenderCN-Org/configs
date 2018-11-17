386fd1bc12d8d85b22523401e0642942e9c1e5f6
moritz <ortz.moritz@gmail.com>
2018-11-17 12:33:23 +0100

    second configuration commit

--- .config/google-chrome/Default/Extensions/fjifglfkcaipnmhngbigdebkoikioend/2.4_0/checkinstall.js
+++
@@ -1,6 +0,0 @@
-var isInstalledNode = document.createElement('div');
-isInstalledNode.id = 'rm-extension-is-installed-note';
-document.body.appendChild(isInstalledNode);
-
-//Double check to make sure it's hidden.
-//document.getElementById('rm-extension-alert-browser').style.display = 'none';
